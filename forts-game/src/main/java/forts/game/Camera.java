04826a59c1a50f2342361d3aef8f395b09180bcb
Francesco De Bernardi <francesco.debernardi@mpolo.it>
2025-05-13 23:28:13 +0200

    Creazione delle fondamenta necessarie per l'interfaccia grafica

--- forts-game/src/main/java/forts/game/Camera.java
+++ forts-game/src/main/java/forts/game/Camera.java
@@ -1,13 +1,51 @@
 package forts.game;
 
-public class Camera {
+import javafx.application.*;
+import javafx.stage.Stage;
+import javafx.scene.Scene;
+import javafx.scene.layout.StackPane;
+import javafx.scene.control.Button;
+import javafx.scene.input.KeyCombination;
+
+public class Camera extends Application {
     private Vector2 position; // Variabile per tenere la posizione della telecamera
+    private Vector2 rootWorldPosition; // La posizione del punto (0, 0) nel mondo, questo valore è basato sulla grandezza dello schermo
     private double zoom; // Variabile per tenere lo zoom della telecamera
 
-    // Costruttore
-    Camera(){
-        this.position = new Vector2(0,0);
+    private Scene rootScene;
+    private StackPane rootPane;
+    
+    private StackPane terrainPane;
+    private StackPane decorationPane;
+    private StackPane buildingsPane;
+    private StackPane backgroundPane;
+
+    // Metodi "costruttori" (poiché JavaFX crea dei thread per la gestione dell'interfaccia, non si può creare un vero e proprio oggetto all'interno del main: Bisogna spostare il main all'interno di questa classe)
+    public void start(Stage primaryStage) {
+        this.position = new Vector2();
+        this.rootWorldPosition = new Vector2();
         this.zoom = 1;
+
+        rootPane = new StackPane();
+
+        terrainPane = new StackPane();
+        decorationPane = new StackPane();
+        buildingsPane = new StackPane();
+        backgroundPane = new StackPane();
+
+        rootPane.getChildren().addAll(terrainPane, decorationPane, buildingsPane, backgroundPane);
+
+        rootScene = new Scene(rootPane);
+        
+        primaryStage.setScene(rootScene);
+        primaryStage.setFullScreen(true);
+        primaryStage.setFullScreenExitHint(new String());
+        primaryStage.setFullScreenExitKeyCombination(KeyCombination.NO_MATCH);
+        primaryStage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
     }
 
     // Metodi get() e set()
@@ -27,10 +65,11 @@ public class Camera {
         this.zoom = newZoom;
     }
 
+    // Metodi classe
     public Vector2 calculateOffset(Vector2 baseVector){
         Vector2 finishedVector;
 
-        finishedVector = (baseVector.multiply(this.zoom)).subtract(this.position);
+        finishedVector = (baseVector.multiply(this.zoom)).subtract(this.position).add(this.rootWorldPosition);
 
         return finishedVector;
     }
