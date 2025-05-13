07c3f095a5ff2402609f482b5a84c8e74e64c604
AlessandroPakula <alessandro.pakula@mpolo.it>
2025-05-13 15:56:43 +0200

    Aggiunta di tre livelli di pannelli in JavaFX con pulsante interattivo e sfondi colorati

--- forts-game/src/main/java/forts/game/Test.java
+++ forts-game/src/main/java/forts/game/Test.java
@@ -1,22 +1,62 @@
 package forts.game;
 
 import javafx.application.Application;
-import javafx.stage.Stage;
+import javafx.scene.Group;
 import javafx.scene.Scene;
-import javafx.scene.layout.StackPane;
+import javafx.scene.layout.*;
+import javafx.scene.paint.Color;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.stage.Stage;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 
 public class Test extends Application {
+    
     @Override
-    public void start(Stage primaryStage) {
-        Button button = new Button("Click Me");
-        button.setOnAction(e -> System.out.println("Button clicked!"));
+    public void start(Stage primaryStage) throws FileNotFoundException {
+        // Livello 1 (Sfondo Rosso)
+        /*FileInputStream inputstream = new FileInputStream("src/main/resources/buildVertexIcon.png");
+        Image image = new Image(inputstream);
+        ImageView imageView = new ImageView(image);
+        imageView.setFitWidth(300);
+        imageView.setFitHeight(250);
+        imageView.setLayoutX(0);
+        imageView.setLayoutY(0);
+        imageView.setPreserveRatio(true);*/
+        Pane layer1 = new Pane();
+        Pane layer2 = new Pane();
+        Pane layer3 = new Pane();
+        Button button = new Button("Ciao");
+        Pane root = new Pane();
+        layer1.setLayoutX(0);
+        layer1.setLayoutY(0);
+        layer1.setStyle("-fx-background-color: red;");
+        layer1.setPrefSize(300, 250);
 
-        StackPane root = new StackPane();
-        root.getChildren().add(button);
+        button.setLayoutX(50);
+        button.setLayoutY(50);
+        button.setStyle("-fx-background-color: blue;");
+        button.setPrefSize(75, 50);
+        layer2.setLayoutX(50);
+        layer2.setLayoutY(50);
+        layer2.setStyle("-fx-background-color: green;");
+        layer2.setPrefSize(150, 125);
+        layer2.getChildren().add(button);
 
-        Scene scene = new Scene(root, 300, 250);
-        primaryStage.setTitle("Test Application");
+        layer3.setLayoutX(0);
+        layer3.setLayoutY(0);
+        layer3.setPrefSize(300, 250);
+        layer3.setStyle("-fx-background-color: blue;");
+        //layer3.getChildren().add(imageView);
+
+        root.getChildren().addAll(layer1, layer2, layer3);
+
+
+        Scene scene = new Scene(root, 900, 600);
+        primaryStage.setTitle("Tre Livelli in JavaFX");
         primaryStage.setScene(scene);
         primaryStage.show();
     }
