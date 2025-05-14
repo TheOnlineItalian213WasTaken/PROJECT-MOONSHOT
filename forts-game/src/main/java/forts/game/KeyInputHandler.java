b8fa8d5ff6de962caaed78edb99ac281d33d10f2
TheOnlineItalian213WasTaken <theonlineitalian213@gmail.com>
2025-05-14 19:35:42 +0200

    Creazione classe ascoltatore / event handler per la tastiera con metodi necessari per il riposizionamento della telecamera

---
+++ forts-game/src/main/java/forts/game/KeyInputHandler.java
@@ -0,0 +1,62 @@
+package forts.game;
+
+import javafx.scene.input.*;
+import javafx.event.*;
+
+// Classe che gestisce ogni evento relativo alla tastiera
+
+public class KeyInputHandler implements EventHandler<KeyEvent> {
+    Camera camera;
+
+    KeyInputHandler(Camera camera) {
+        this.camera = camera;
+    }
+
+    public void handle(KeyEvent event) {
+        if(event.getEventType() == KeyEvent.KEY_PRESSED) {
+            movementStart(event);
+        } else {
+            movementEnd(event);
+        }
+    }
+
+    public void movementStart(KeyEvent keyEvent) {
+        Vector2 movementTarget;
+        KeyCode key = keyEvent.getCode();
+
+        if(key == KeyCode.UP) {
+            movementTarget = new Vector2(0, 5);
+        } else if(key == KeyCode.DOWN) {
+            movementTarget = new Vector2(0, -5);
+        } else if(key == KeyCode.LEFT) {
+            movementTarget = new Vector2(-5, 0);
+        } else if(key == KeyCode.RIGHT) {
+            movementTarget = new Vector2(5, 0);
+        } else {
+            return;
+        }
+
+        if(keyEvent.isShiftDown()) {
+            movementTarget.multiply(2);
+        }
+
+        camera.setCameraVelocity(movementTarget);
+    }
+
+    public void movementEnd(KeyEvent keyEvent) {
+        Vector2 movementTarget;
+        KeyCode key = keyEvent.getCode();
+
+        if(key == KeyCode.UP || key == KeyCode.DOWN || key == KeyCode.LEFT || key == KeyCode.RIGHT) {
+            movementTarget = new Vector2(0, 0);
+        } else {
+            return;
+        }
+
+        camera.setCameraVelocity(movementTarget);
+    }
+
+    public void movementHandle() {
+        this.camera.setPosition(this.camera.getPosition().add(this.camera.getCameraVelocity()));
+    }
+}
