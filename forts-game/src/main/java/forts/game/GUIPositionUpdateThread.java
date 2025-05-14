d311416a7e1e332458c24c7687dee95966b7db6d
TheOnlineItalian213WasTaken <theonlineitalian213@gmail.com>
2025-05-14 21:17:29 +0200

    Creazione classe per l'aggiornamento delle posizioni degli elementi dell'interfaccia grafica

--- forts-game/src/main/java/forts/game/GUIPositionUpdateThread.java
+++ forts-game/src/main/java/forts/game/GUIPositionUpdateThread.java
@@ -1,5 +1,32 @@
 package forts.game;
 
-public class GUIPositionUpdateThread {
+import java.util.ArrayList;
+import javafx.application.*;
 
+// Thread che gestisce il ciclo con il quale
+
+public class GUIPositionUpdateThread extends Thread {
+    Camera camera;
+
+    GUIPositionUpdateThread(Camera camera) {
+        this.camera = camera;
+    }
+
+    public void run() {
+        while(true) {
+            updateVertices();
+        }
+    }
+
+    public void updateVertices() {
+        int i;
+        ArrayList vertices;
+
+        vertices = this.camera.getMainFort().getVertices();
+        for (i = 0; i < vertices.size(); i++) {
+            Vertex currVertex = (Vertex) vertices.get(i);
+            VertexPositionUpdateThread newThread = new VertexPositionUpdateThread(currVertex, this.camera);
+            Platform.runLater(newThread);
+        }
+    }
 }
