d9fc9a06203b924d3dbe14dbe429d12ed6503141
TheOnlineItalian213WasTaken <theonlineitalian213@gmail.com>
2025-05-15 13:11:26 +0200

    Implementazione interfaccia drawable senza rotazione dinamica in base all'angolo tra due vertici

--- forts-game/src/main/java/forts/game/Connection.java
+++ forts-game/src/main/java/forts/game/Connection.java
@@ -1,11 +1,15 @@
 package forts.game;
 
 import java.lang.Math;
+import javafx.scene.image.*;
+import javafx.scene.transform.*;
 
 // Classe che gestisce le connessioni tra vertici di costruzione
 
 public class Connection implements Drawable {
     private Vertex[] vertices; // I due vertici della connessione sono tenuti qui dentro
+
+    private ImageView sprite;
 
     private Material material;
     private double weight;
@@ -59,6 +63,35 @@ public class Connection implements Drawable {
         }
 
         return otherVertex;
+    }
+
+    public void draw(Camera camera) {
+        this.sprite = new ImageView(this.material.getSpriteDirectory()); // Creazione iniziale dell'elemento grafico per il vertice
+        this.sprite.setPreserveRatio(false);
+
+        camera.getBuildingsPane().getChildren().add(sprite);
+
+        this.update(camera);
+    }
+
+    public void update(Camera camera) {
+        double relativeScale;
+        Vector2 absolutePosition, relativePosition, sizeOffset;
+        Scale scale;
+
+        // Modifica della grandezza in base allo zoom della telecamera
+        relativeScale = (this.baseLength / 500) * camera.getZoom();
+        scale = new Scale(camera.getZoom(), relativeScale, 0, 0);
+        this.sprite.getTransforms().add(scale);
+
+        // Riposizionamento del gui dinamico in base alla posizione root del mondo e posizizone della telecamera
+        sizeOffset = new Vector2((100 * camera.getZoom()), (250 * camera.getZoom() * (relativeScale / camera.getZoom())));
+        absolutePosition = (this.vertices[0].getPosition().add(this.vertices[1].getPosition())).divide(2);
+        relativePosition = camera.calculateOffset(absolutePosition);
+        relativePosition = relativePosition.subtract(sizeOffset);
+        this.sprite.setLayoutX(relativePosition.getX());
+        this.sprite.setLayoutY(relativePosition.getY());
+        System.out.println("" + relativePosition + relativePosition.add(sizeOffset));
     }
     
 }
