d491743b4f091544c44f09f3952fe557c607b25d
pak-man2 <alessandropakula@gmail.com>
2025-05-18 09:56:16 +0200

    Aggiunta della descrizione nella classe material e tolta dalla classe Wood e dalla classe Iron perchè superfluo

--- forts-game/src/main/java/forts/game/Iron.java
+++ forts-game/src/main/java/forts/game/Iron.java
@@ -1,21 +1,10 @@
 package forts.game;
 
 public class Iron extends Material {
-    String description;
     // Costruttore
     public Iron() {
         super("iron", 0.5, 0.5, 0.5, 0.5, 0.5);
     }
-    // metodi get e set
-    @Override
-    public String getName() {
-        return "Iron";
-    }
-
-    public String getDescription() {
-        return "A basic building material.";
-    }
-    public void setDescription(String description) {
-        this.description = description;
-    }
+    
+    
 }
--- forts-game/src/main/java/forts/game/Wood.java
+++ forts-game/src/main/java/forts/game/Wood.java
@@ -1,22 +1,11 @@
 package forts.game;
 
 public class Wood extends Material {
-    String descripion;
+    
     // Costruttore
     public Wood() {
         super("wood", 0.5, 0.5, 0.5, 0.5, 0.5, "woodSprite.png");
     }
 
-    @Override
-    public String getName() {
-        return "Wood";
-    }
 
-    
-    public String getDescription() {
-        return "A basic building material.";
-    }
-    public void setDescription(String description) {
-        this.descripion = description;
-    }
 }
